0215774eb459783b70387584b166a0a6c87a71e4
Indrani Chiravuri <indranichiravuri916@gmail.com>
2019-02-21 10:52:17 +0530

    sr

--- C-Assignments/Assignment - 4/StringRotation.cpp
+++ C-Assignments/Assignment - 4/StringRotation.cpp
@@ -1,12 +1,13 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 
 int main() {
 	char s[50];
 	scanf("%s",s);
-	for(int i=0;i<strlen(s);i++) {
+	for(int i = 0; i < strlen(s); i++) {
 		printf("%s",s+i);
-		for(int j=0;j<i;j++)
+		for(int j = 0;j < i; j++)
 			printf("%c",s[j]);
 		printf("\n");
 	}
